9a7a2452dd37dbfd4e33d6179a83dcc6458bfec1
ansiblerg <rishabhgoyal0001@gmail.com>
2020-06-19 16:11:49 +0530

    Updates

---
+++ precache-manifest.bdb82fdf23c96997ec7c41b39195ddec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a83e09048509211de10f3de768db1f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10b3190f9b0b7b614b87",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "557183a5f2d70a2d553c",
+    "url": "/static/css/main.9a6dc019.chunk.css"
+  },
+  {
+    "revision": "10b3190f9b0b7b614b87",
+    "url": "/static/js/2.4b6b2329.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.4b6b2329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "557183a5f2d70a2d553c",
+    "url": "/static/js/main.59d2066b.chunk.js"
+  },
+  {
+    "revision": "e3f6cca2499cc0ccc243",
+    "url": "/static/js/runtime-main.b2109bab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
